7635378ef18e882f5a31b1f256ac2fc792fef4a0
Khada-Jhin <1099422647@qq.com>
2017-10-31 22:57:05 +0800

    change

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,60 +1,67 @@
-window.onload = function() {
+	window.onload = function() {
 
-	var bts = document.getElementsByName("buttons");
-	for (var i = bts.length - 1; i >= 0; i--) {
-		bts[i].onclick = function() {
-			exec(event.target.textContent);
-		}
-	}
+			var bts = document.getElementsByName("buttons");
 
-
-	function exec(str) {
-		//alert(str);
-		//if (str.length>2) str=""
-		var old = document.getElementById("output").value;
-		if (str == "←") {
-			old = old.slice(0, old.length - 1);
-		} else if (str == "CE") {
-			old = "";
-		} else if (str == "=") {
-			try {
-				old = eval(old).toString();
-				if (old.match('.') != null) {
-					old = parseFloat(old).toFixed(12).toString();
-					old = parseFloat(old);
+			for (var i = bts.length - 1; i >= 0; i--) {
+				bts[i].onclick = function() {
+					exec(event.target.textContent);
 				}
+			}
+
+			function exec(str) {
+				//alert(str);
+				//if (str.length>2) str=""
+				var old = document.getElementById("output").value;
+				if (str == "←") {
+					old = old.slice(0, old.length - 1);
+				} else if (str == "CE") {
+					old = "";
+				} else if (str == "=") {
+					try {
+						old = eval(old).toString();
+						if (old.match('.') != null) {
+							old = parseFloat(old).toFixed(12).toString();
+							old = parseFloat(old);
+						}
 
 
-			} catch (exception) {
-				alert("请输入正确公式");
-				old = "";
+					} catch (exception) {
+						alert("请输入正确公式");
+						old = "";
+					}
+				} else if (str == "/" || str == "*" || str == "+" || str == "-") {
+					try {
+						var temp = old.substr(old.length - 1, 1);
+						if (temp == "/" || temp == "*" || temp == "+" || temp == "-") throw "error";
+						old += str;
+					} catch (exception) {
+						alert("请输入正确公式");
+						old = "";
+					}
+				} else if (str == "0") {
+					try {
+						var temp = old.substr(old.length - 1, 1);
+						if (temp == "/") throw "error";
+						old += str;
+					} catch (exception) {
+						alert("不能除以0");
+						old = "";
+					}
+				} else {
+					old += str;
+				}
+				try {
+					if (old == "Infinity" || old == "NaN") throw "error";
+					show(old);
+				} catch (exception) {
+					alert("不能除以0");
+					old = "";
+				}
+				show(old);
 			}
-		} else if (str == "/" || str == "*" || str == "+" || str == "-") {
-			try {
-				var temp = old.substr(old.length - 1, 1);
-				if (temp == "/" || temp == "*" || temp == "+" || temp == "-") throw "error";
-				old += str;
-			} catch (exception) {
-				alert("请输入正确公式");
-				old = "";
+
+			function show(result) {
+				//result=parseFloat(result);
+				document.getElementById("output").value = result;
 			}
-		} else if (str == "0") {
-			try {
-				var temp = old.substr(old.length - 1, 1);
-				if (temp == "/") throw "error";
-				old += str;
-			} catch (exception) {
-				alert("不能除以0");
-				old = "";
-			}
-		} else {
-			old += str;
-		}
-		show(old);
 	}
-
-	function show(result) {
-		//result=parseFloat(result);
-		document.getElementById("output").value = result;
-	}
-}
